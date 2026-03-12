46e940707a74fda80332d019a49c2cafa7f91fb2
Ubuntu <ubuntu@ip-172-31-11-41.ap-south-1.compute.internal>
2026-03-12 10:34:42 +0000

    chore: remove dead commented code in app.js; update About page content

--- Frontend/inventory_management_system/src/components/About.js
+++ Frontend/inventory_management_system/src/components/About.js
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className='container-fluid p-5'>
         <h1>Inventory Management System - MERN CRUD App</h1>
-        <p>...</p>
+        <p>A simple CRUD application to manage product inventory. Supports adding, viewing, updating, and deleting products by barcode.</p>
     </div>
   )
 }
